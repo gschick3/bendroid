824e3c0ed8ee81cae4561c0d84ec88a1a8106109
gschick3 <gschick3@gmail.com>
2023-09-15 16:10:22 -0500

    create script to download Androzoo APK list

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bendroid
 
 Using Benford's Law to detect Android malware.
+
+## Setup
+1. Create `.ben` file in the format of `.example.ben`
+2. Run the `download_latest_apks.py` script to get latest csv of all Androzoo APKs (this may take a couple hours)
---
+++ download_latest_apks.py
@@ -0,0 +1,28 @@
+from urllib.request import urlretrieve
+import progressbar
+import gzip, shutil
+import os
+
+url = 'https://androzoo.uni.lu/static/lists/latest_with-added-date.csv.gz'
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'APKs', 'latest_with-added-date.csv.gz')
+
+pbar = None
+
+def show_progress_bar(block_num, block_size, total_size):
+    global pbar
+    if pbar is None:
+        pbar = progressbar.ProgressBar(max_value=total_size)
+        pbar.start()
+
+    downloaded = block_num * block_size
+    if downloaded < total_size:
+        pbar.update(downloaded)
+    else:
+        pbar.finish()
+        pbar = None
+
+path, headers = urlretrieve(url, filename, show_progress_bar)
+
+with gzip.open(path, 'rb') as f_in:
+    with open(path.rstrip('.gz')) as f_out:
+        shutil.copyfileobj(f_in, f_out)
